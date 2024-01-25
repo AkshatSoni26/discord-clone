f38c1e4e3292c91047d9f5363cd315e9d6faa9e7
AkshatSoni26 <135410361+AkshatSoni26@users.noreply.github.com>
2024-01-25 12:59:36 +0530

    uploadthing error fix.

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,6 +1,7 @@
 import { generateComponents } from "@uploadthing/react";
+import { OurFileRouter } from "../app/api/uploadthing/core";
  
-import type { OurFileRouter } from "@/app/api/uploadthing/core";
+
  
 export const { UploadButton, UploadDropzone, Uploader } =
   generateComponents<OurFileRouter>();
